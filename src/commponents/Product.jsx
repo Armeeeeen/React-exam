c42b5049410aa8dc6a67e2c7a62a576246c33889
armeeeeen <armgamer4@gmail.com>
2023-08-15 22:19:51 +0400

    back button added

--- src/commponents/Product.jsx
+++ src/commponents/Product.jsx
@@ -2,7 +2,6 @@ import React, { useEffect, useState } from 'react';
 import { NavLink, useParams } from 'react-router-dom';
 import './css/single.css'
 import { useDispatch } from 'react-redux';
-// import { addItem } from '../redux/actions';
 import {addItem} from '../redux/actions/index'
 
 function ProductDetails() {
@@ -27,6 +26,9 @@ function ProductDetails() {
 
   return (
   <div className='single'>
+    <div>
+      <NavLink to={'/products'}><img src="https://www.freeiconspng.com/thumbs/return-button-png/back-undo-return-button-png-5.png" alt=""  className='back-button'/></NavLink>
+    </div>
       <h1>{product.title}</h1>
     <div id='container'>
       <div className="left">
